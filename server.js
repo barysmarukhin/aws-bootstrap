4a823226e79e624a98fa6548d3406af459322d4e
Barys Marukhin <barys_marukhin@epam.com>
2023-10-29 15:58:24 +0400

    Add CodeDeploy/CodeBuild/CodePipeline

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { hostname } = require('os');
 const http = require('http');
 
+console.log({  hostname});
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
