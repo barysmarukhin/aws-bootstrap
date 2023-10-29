6e2d5d179205392716b84339b96d42ac7e2b7c16
Barys Marukhin <barys_marukhin@epam.com>
2023-10-29 17:07:46 +0400

    Attempt to fix CodeDeploy

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const { hostname } = require('os');
 const http = require('http');
 
+const message = 'Hello Cloud\n';
+const port = 8080;
+
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, Cloud!');
+    res.end(message);
 });
 
-server.listen(8080, hostname, () => {
-    console.log('Server running at http://localhost:8080/');
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname()}:${port}/`);
 });
