603a24e6ce1e985bae6f1ec73de7f09914400dc1
Barys Marukhin <barys_marukhin@epam.com>
2024-01-31 15:56:21 +0400

    Add hostname to hello world message

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { hostname } = require('os');
 const http = require('http');
 
-const message = 'Hello Cloud, Please RUN RUN\n';
+const message = `Hello World from ${hostname()}\n`;
 const port = 8080;
 
 const server = http.createServer((req, res) => {
